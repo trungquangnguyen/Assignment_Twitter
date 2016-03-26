bb87a61396889b8022377ff5e27acba7912e65a1
nguyen trung quang <trunhquang@gmail.com>
2016-03-26 11:14:28 +0700

    update Bridging-Header

---
+++ Twitter/TwitterClient.swift
@@ -0,0 +1,12 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by nguyen trung quang on 3/26/16.
+//  Copyright © 2016 trunhquang.com. All rights reserved.
+//
+
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
